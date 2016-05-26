2ddd19b3c3f2e6b5a9d4c2d9780961bc306d026a
saposki <saposki@gmail.com>
2016-05-25 18:20:34 -0700

    created pages model created pages attributes

---
+++ ROADMAP.md
@@ -0,0 +1,17 @@
+
+
+pages(home)
+   ATTRIBUTES
+   -  logo  (img)
+   -  title (text)
+   -  description (text)
+      ABOUT US (title)
+         - description (text)
+      WHAT WE DO (title)
+         - description - (text)
+portfolios
+   ATTRIBUTES
+teams
+   ATTRIBUTES
+contacts
+   ATTRIBUTES
--- base/src/pages/models.py
+++ base/src/pages/models.py
@@ -4,8 +4,22 @@ from django.db import models
 
 # Create your models here.
 class Page(models.Model):
-    title = models.CharField(max_length=120)
-    content = models.TextField()
+    #logo
+    image = models.ImageField(upload_to='updload_location',
+        null=True, blank=True,
+        width_field='width_field',
+        height_field='height_field')
+    height_field = models.IntegerField(default=0)
+    width_field = models.IntegerField(default=0)
+    #title
+    title = models.CharField(max_length=120, default='title')
+    #description
+    description = models.TextField(max_length=240, default='description')
+    #about_us
+    about_us = models.TextField(max_length=240, default='about us')
+    #what_we_do
+    what_we_do = models.TextField(max_length=240, default='what we do')
+
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
 
